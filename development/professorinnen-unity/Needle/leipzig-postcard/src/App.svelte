ad49a7d5f5a198b662d9852af4c2a65cbf17fc73
llllkatjallll <katja.rempel@yahoo.de>
2025-04-10 00:28:47 +0200

    added postcard animation and shadowplane

---
+++ development/professorinnen-unity/Needle/leipzig-postcard/src/App.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    import NeedleEngine from "./NeedleEngine.svelte";
+</script>
+
+<NeedleEngine></NeedleEngine>
---
+++ development/professorinnen-unity/Needle/leipzig-postcard/src/NeedleEngine.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+
+    /**
+     * The src can be a remote url to a GLB (or local file)
+    */
+    export let src: string | undefined = undefined;
+
+    /**
+     * Skybox Image can be assigned via a URL e.g. https://dl.polyhaven.org/file/ph-assets/HDRIs/exr/2k/tief_etz_2k.exr
+    */
+    export let skyboxImage: string | undefined = undefined;
+    /**
+     * Environment Image can be assigned via a URL e.g. https://dl.polyhaven.org/file/ph-assets/HDRIs/exr/2k/tief_etz_2k.exr
+    */
+    export let environmentImage: string | undefined = undefined;
+</script>
+
+<!-- find all attributes in the documentation at https://engine.needle.tools/docs/reference/needle-engine-attributes.html-->
+<needle-engine {src} skybox-image="{skyboxImage}" environment-image="{environmentImage}">
+    <slot />
+</needle-engine>
+
+<style>
+</style>
